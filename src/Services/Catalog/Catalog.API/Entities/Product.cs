8456abe5087cf01fe6c16c04350287654862bd1b
Shakeer <shakeer.ahmadd@outlook.com>
2022-01-15 16:35:11 +0530

    product entity added to Catalog .API

---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("Name")]
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public string Summary { get; set; } = string.Empty ;
+        public string Description { get; set; } = string.Empty;
+        public string? ImageFile { get; set; }
+        public decimal Price { get; set; } = 0.0m;
+    }
+}
